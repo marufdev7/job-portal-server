334a245a91001f6678335a189709cb48f66042ab
mrfhmd7 <mrf.hmd7@gmail.com>
2025-07-31 23:58:45 +0600

    dot env add

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
-const uri = `mongodb+srv://job_hunter:QmyKPKZ5p1FEsXWh@cluster0.jworznu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jworznu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -33,7 +31,7 @@ async function run() {
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
