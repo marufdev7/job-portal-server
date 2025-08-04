a580de063594e3a0c3724ceb036279a64eb18a5c
mrfhmd7 <mrf.hmd7@gmail.com>
2025-08-04 23:04:59 +0600

    get applicant jobs

--- index.js
+++ index.js
@@ -47,7 +47,14 @@ async function run() {
 
         // Job application apis
 
-        app.post('/job-application', async (req, res) => {
+        app.get('/job-applications', async (req, res) => {
+            const email = req.query.email;
+            const query = { applicant_email: email };
+            const result = await jobApplicationCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.post('/job-applications', async (req, res) => {
             const application = req.body;
             const result = await jobApplicationCollection.insertOne(application);
             res.send(result)
