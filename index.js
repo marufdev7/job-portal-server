21cb347229d7ea0a3639a1b9e091d352c367035c
mrfhmd7 <mrf.hmd7@gmail.com>
2025-08-29 21:14:59 +0600

    sort by salary

--- index.js
+++ index.js
@@ -91,12 +91,19 @@ async function run() {
 
         app.get('/jobs', logger, async (req, res) => {
             const email = req.query.email;
+            const sort = req.query?.sort;
             let query = {};
+            let sortQuery = {};
+
             if (email) {
                 query = { hr_email: email }
             }
 
-            const cursor = jobsCollection.find(query);
+            if (sort == "true") {
+                sortQuery = { "salaryRange.min": -1 }
+            }
+
+            const cursor = jobsCollection.find(query).sort(sortQuery);
             const result = await cursor.toArray();
             res.send(result);
         });
